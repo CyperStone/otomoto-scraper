2e0abd327819573f20693e17022b07260cd34643
CyperStone <szymoncyperski@gmail.com>
2023-04-28 15:53:39 +0200

    Enable providing scraping details via program arguments

--- scrape_all_offers.py
+++ scrape_all_offers.py
@@ -1,9 +1,15 @@
+import argparse
 import config
 from scraper import OtomotoScraper
 
 
 if __name__ == '__main__':
-    results_dir = './data'
-    scraper = OtomotoScraper(config.website, config.scraper)
+    parser = argparse.ArgumentParser(description='Run scraping all car offers from Otomoto.pl')
+    parser.add_argument('--results_dir', dest='results_dir', type=str, nargs='?', default=None,
+                        help='Path to directory where results have to be saved')
+    parser.add_argument('--to_eng', dest='to_eng', type=str, action=argparse.BooleanOptionalAction, default=False,
+                        help='Type of camera the videos were recorded')
+    args = parser.parse_args()
 
-    _ = scraper.scrape_all_offers(results_dir)
+    scraper = OtomotoScraper(config.website, config.scraper)
+    _ = scraper.scrape_all_offers(results_dir=args.results_dir, to_eng=args.to_eng)
