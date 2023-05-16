c5e1ee9382e6ad6e45dfd16acdaeaf20d199312c
CyperStone <szymoncyperski@gmail.com>
2023-05-16 13:29:57 +0200

    Change description of input argument "to_eng"

--- scrape_all_offers.py
+++ scrape_all_offers.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('--results_dir', dest='results_dir', type=str, nargs='?', default=None,
                         help='Path to directory where results have to be saved')
     parser.add_argument('--to_eng', dest='to_eng', type=str, action=argparse.BooleanOptionalAction, default=False,
-                        help='Type of camera the videos were recorded')
+                        help='Whether to translate results to english')
     args = parser.parse_args()
 
     scraper = OtomotoScraper(config.website, config.scraper)
